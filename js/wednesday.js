5cf16db905d132c94ba7d097b3b459608141136f
Sarah Joy Calpo <sajoy.pc@gmail.com>
2017-10-24 14:47:13 -0700

    prep Wednesday demo files

---
+++ js/wednesday.js
@@ -0,0 +1,31 @@
+/* 1. Switch statement */
+// TODO prompt user for their fav color, and depending on the color, alert a fruit of that color
+
+
+
+
+
+/* 2. Loops! */
+
+// for loop
+
+// while loop
+
+// do while loop
+
+
+
+/* polling commits by students */
+// TODO prompt students and ask how many commits they made yesterday
+
+
+// TODO use a *for loop* if we know how many times to repeat
+// (ie, we have an array of students)
+
+
+// TODO use a *while loop* if we don't know how many times to repeat && don't need to run at least once
+// (we only want to collect if the variable today === 'Monday')
+
+
+// TODO use a *do while loop* if we don't know how many times to repeat && do need to run at least once
+// (we want to collect commits until we reach 100)
