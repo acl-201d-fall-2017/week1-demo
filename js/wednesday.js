b42bd873677751cea15478ba520e14a602bc2bed
Sarah Joy Calpo <sajoy.pc@gmail.com>
2017-10-25 08:40:00 -0700

    update wednesday notes

--- js/wednesday.js
+++ js/wednesday.js
@@ -13,7 +13,9 @@
 
 // do while loop
 
+const go = '?';
 
+// const hot;
 
 /* polling commits by students */
 // TODO prompt students and ask how many commits they made yesterday
@@ -23,9 +25,9 @@
 // (ie, we have an array of students)
 
 
-// TODO use a *while loop* if we don't know how many times to repeat && don't need to run at least once
-// (we only want to collect if the variable today === 'Monday')
-
-
 // TODO use a *do while loop* if we don't know how many times to repeat && do need to run at least once
 // (we want to collect commits until we reach 100)
+
+
+// TODO use a *while loop* if we don't know how many times to repeat && don't need to run at least once
+// (we only want to collect if the variable today === 'Monday')
