df49777946073a39f1e02dc549af5760ab859f32
Sarah Joy Calpo <sajoy.pc@gmail.com>
2017-10-26 11:19:16 -0700

    interact with user kindly

--- js/thursday.js
+++ js/thursday.js
@@ -1,5 +1,8 @@
 'use strict';
 
+alert('hi world!')
+
+// annoy();
 
 /* anatomy of a function */
 
@@ -14,12 +17,73 @@ function shout (word) {
     return word.toUpperCase() = the function's return statement
 */
 
+/* changing up the order! */
+announceBreakfast();
+console.log('breakfast');
+console.log('lunch');
+console.log('dinner');
+
+
+
+function announceBreakfast () {
+    console.log('breakfast');
+}
+
+function announceLunch () {
+    console.log('lunch');
+}
+
+function announceDinner() {
+    console.log('dinner');
+}
+
+
+announceBreakfast();
+announceLunch();
+announceDinner();
+announceBreakfast();
+
+
+
+/* Make things DRY!!! */
+
+function announce (meal) {
+    console.log(meal);
+}
+
+announce('third breakfast');
+
+
+
 
 
 /* function declaration vs function expression */
 
-var collectCats = function () {
+// function expression can only be called after being saved in a variable
+const collectCats = function () {
     console.log('Lure them with tuna');
+};
+
+const appease = function () {
+    const name = prompt('name pls?');
+    alert('no, really i am super super sorry ' + name );
+};
+
+// appease();
+
+
+// function declarations! can be used before its declared in the code
+const variableName = 'someting';
+
+function nameAFunction (name) {
+    // code that names functions
+}
+
+function annoy () {
+    const favLetter = prompt('what is your favorite letter?');
+    for (let i = 0; i < 5; i ++) {
+        alert(favLetter.toUpperCase());
+    }
 }
 
 function collectDogs () {
@@ -30,7 +94,8 @@ function collectDogs () {
 
 /* parameter and variable scope */
 
-function kind (word, alertType) {
+function ask (word, alertType) {
+    
     const phrase = 'please ' + word.toLowerCase() + ' and thank you, kindly!';
     if (alertType === 'confirm') {
         confirm(phrase);
@@ -39,4 +104,23 @@ function kind (word, alertType) {
     } else {
         alert(phrase);
     }
+
+    console.log( 'inside ask function ------------ ');
+    console.log( 'word is:' + word );
+    console.log( 'alertType is:' + alertType );
+    console.log( 'phrase is:' + phrase );
+
+    return 'lol';
 }
+
+console.log(phrase);
+const moon = ask('take a break', 'confirm');
+console.log( 'the phrase was: ' + moon );
+
+// const word = 'defined outside';
+
+console.log( 'outside ask function ------------ ');
+console.log( 'word is:' + word );
+console.log( 'alertType is:' + alertType );
+console.log( 'phrase is:' + phrase );
+
