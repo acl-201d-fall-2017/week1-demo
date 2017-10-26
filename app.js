a7152739e58121c96aab08e2f241103df9376a77
Sarah Joy Calpo <sajoy.pc@gmail.com>
2017-10-26 11:26:14 -0700

    clean up some code

--- app.js
+++ app.js
@@ -1,28 +1,27 @@
 'use strict';
 
 let score;
+askForFavSeason();
 
-const seasons = ['fall','autumn','summer','winter','spring'];
-// const vacations = ['beach','mountains'];
 
-// TODO ask user their favSeason until they answer correctly
-let favSeason = prompt( 'What is your favorite season?' ); // 'winter'
+function askForFavSeason () {
+    const seasons = ['fall','autumn','summer','winter','spring'];
+    
+    // TODO ask user their favSeason until they answer correctly
+    let favSeason = prompt( 'What is your favorite season?' ); // 'winter'
 
-while ( !seasons.includes(favSeason) ) {
-    favSeason = prompt('No, really, what is your fav season??');
+    while ( !seasons.includes(favSeason) ) {
+        favSeason = prompt('No, really, what is your fav season??');
+    }
+
+    score = seasons.indexOf(favSeason); // 3
 }
 
-score = seasons.indexOf(favSeason); // 3
-
-
-
-// console.log(seasons.includes(favSeason));
-
+// const vacations = ['beach','mountains'];
 
 // TODO ask user their favVacation until they answer correctly
 // const favVacation = prompt( 'Which vacation spot do you prefer? Beach or mountains?' );
 
-// TODO return a function based on a user's score (using their answer's index)
 if (score === 0) {
     alert( 'you are parseInt()' );
 } else if (score < 3) {
