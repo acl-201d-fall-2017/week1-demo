d88ea7cb30c7c0cee21d9e49396d635bc9004a4b
Sarah Joy Calpo <sajoy.pc@gmail.com>
2017-10-26 11:38:35 -0700

    added question about favorite vacation

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 'use strict';
 
 let score;
-askForFavSeason();
 
+const seasons = ['fall','autumn','summer','winter','spring'];
+askForFav( seasons, 'What is your favorite season?', 'No, really, what is your fav season??' );
 
-function askForFavSeason () {
-    const seasons = ['fall','autumn','summer','winter','spring'];
-    
-    // TODO ask user their favSeason until they answer correctly
-    let favSeason = prompt( 'What is your favorite season?' ); // 'winter'
+const vacations = ['beach','mountains'];
+askForFav( vacations, 'What is your fav vacation spot?', 'Choose: beach or mountains?' );
 
-    while ( !seasons.includes(favSeason) ) {
-        favSeason = prompt('No, really, what is your fav season??');
+function askForFav (possibleAns, question, secondQuestion) {
+    let answer = prompt( question );
+
+    while ( !possibleAns.includes(answer) ) {
+        answer = prompt( secondQuestion );
     }
 
-    score = seasons.indexOf(favSeason); // 3
+    score = possibleAns.indexOf(answer);
 }
 
-// const vacations = ['beach','mountains'];
 
 // TODO ask user their favVacation until they answer correctly
 // const favVacation = prompt( 'Which vacation spot do you prefer? Beach or mountains?' );
