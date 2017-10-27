92e906011560c98156354c4efab636fee86e75a2
Sarah Joy Calpo <sajoy.pc@gmail.com>
2017-10-26 17:35:28 -0700

    add more questions to the quiz

--- app.js
+++ app.js
@@ -1,31 +1,35 @@
 'use strict';
 
-let score;
+let score = 0;
 
 const seasons = ['fall','autumn','summer','winter','spring'];
 askForFav( seasons, 'What is your favorite season?', 'No, really, what is your fav season??' );
 
-const vacations = ['beach','mountains'];
-askForFav( vacations, 'What is your fav vacation spot?', 'Choose: beach or mountains?' );
+const vacations = ['beach','mountains','forest','lake'];
+askForFav( vacations, 'What is your fav vacation spot?', 'Okay, but if you had to choose: beach or mountains?' );
+
+const pets = ['cat','dog','mouse','iguana','goat','rock'];
+askForFav( pets, 'What is your fav pet animal?', 'Hmm, never heard of that! Try another.' );
+
+const language = ['python','ruby','java','javascript'];
+askForFav( language, 'What is your fav programming language?', '*ahem* I think you mean JavaScript.' );
 
 function askForFav (possibleAns, question, secondQuestion) {
-    let answer = prompt( question );
+    let answer = prompt( question ).toLowerCase();
 
     while ( !possibleAns.includes(answer) ) {
-        answer = prompt( secondQuestion );
+        answer = prompt( secondQuestion ).toLowerCase();
     }
 
-    score = possibleAns.indexOf(answer);
+    score += possibleAns.indexOf(answer);
 }
 
-
-// TODO ask user their favVacation until they answer correctly
-// const favVacation = prompt( 'Which vacation spot do you prefer? Beach or mountains?' );
-
 if (score === 0) {
-    alert( 'you are parseInt()' );
-} else if (score < 3) {
-    alert( 'you are .toLowerCase()' );
-} else if (score >= 3) {
-    alert( 'you are console.log()' );
+    alert('you are parseInt()');
+} else if (score <= 7) {
+    alert('you are .toLowerCase()');
+} else if (score <= 13) {
+    alert('you are console.log()');
+} else if (score > 13) {
+    alert('you are alert()');
 }
